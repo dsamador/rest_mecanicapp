52d40859358b0600dabcf8710ddaec044d6f699d
David Amador <54488581+dsamador@users.noreply.github.com>
2021-04-26 18:47:47 -0500

    Se añade la funcionalidad de agregar registros en tipos de lavados

--- frontend/src/components/TipoLavado.vue
+++ frontend/src/components/TipoLavado.vue
@@ -12,44 +12,37 @@
                 <v-text-field class="text-xs-center" v-model="search" append-icon="search" 
                 label="Búsqueda" single-line hide-details></v-text-field>
                 <v-spacer></v-spacer>
+
                 <v-dialog v-model="dialog" max-width="500px">
-                <template v-slot:activator="{ on }">
-                    <v-btn color="primary" dark class="mb-2" v-on="on">Nuevo</v-btn>
-                </template>
-                <v-card>
-                    <v-card-title>
-                    <span class="headline">{{ formTitle }}</span>
-                    </v-card-title>
+                    <template v-slot:activator="{ on }">
+                        <v-btn color="primary" dark class="mb-2" v-on="on">Nuevo</v-btn>
+                    </template>
+                    <v-card>
+                        <v-card-title>
+                        <span class="headline">{{ formTitle }}</span>
+                        </v-card-title>
+            
+                        <v-card-text>
+                            <v-container grid-list-md>
+                                <v-layout wrap>
+                                    <v-flex xs12 sm12 md12>
+                                        <v-text-field v-model="nombre" label="Nombre"></v-text-field>
+                                    </v-flex>                                    
+                                    <v-flex xs12 sm12 md12>
+                                        <v-text-field v-model="descripcion" label="Descripción"></v-text-field>
+                                    </v-flex>                                    
+                                </v-layout>
+                            </v-container>
+                        </v-card-text>
         
-                    <v-card-text>
-                    <v-container grid-list-md>
-                        <v-layout wrap>
-                        <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.calories" label="Calories"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.fat" label="Fat (g)"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.carbs" label="Carbs (g)"></v-text-field>
-                        </v-flex>
-                        <v-flex xs12 sm6 md4>
-                            <v-text-field v-model="editedItem.protein" label="Protein (g)"></v-text-field>
-                        </v-flex>
-                        </v-layout>
-                    </v-container>
-                    </v-card-text>
-        
-                    <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn color="blue darken-1" flat @click="close">Cancelar</v-btn>
-                    <v-btn color="blue darken-1" flat @click="save">Guardar</v-btn>
-                    </v-card-actions>
-                </v-card>
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+                            <v-btn color="blue darken-1" flat @click="close">Cancelar</v-btn>
+                            <v-btn color="blue darken-1" flat @click="guardar">Guardar</v-btn>
+                        </v-card-actions>
+                    </v-card>
                 </v-dialog>
+
             </v-toolbar>
 
             <v-data-table
@@ -59,15 +52,15 @@
                 class="elevation-1"
             >
                 <template v-slot:items="props">
-                <td>{{ props.item.nombre }}</td>
-                <td>{{ props.item.descripcion }}</td>                            
-                <td>
-                    <v-icon small class="mr-2" @click="editItem(props.item)"> edit </v-icon>
-                    <v-icon small class="mr-2" @click="deleteItem(props.item)"> delete </v-icon>                    
-                </td>
+                    <td>{{ props.item.nombre }}</td>
+                    <td>{{ props.item.descripcion }}</td>                            
+                    <td>
+                        <v-icon small class="mr-2" @click="editItem(props.item)"> edit </v-icon>
+                        <v-icon small class="mr-2" @click="deleteItem(props.item)"> delete </v-icon>                    
+                    </td>
                 </template>
                 <template v-slot:no-data>
-                <v-btn color="primary" @click="initialize">Reset</v-btn>
+                    <v-btn color="primary" @click="listar()">Resetear</v-btn>
                 </template>
             </v-data-table>
         </v-flex>
@@ -87,33 +80,21 @@
                     { text: 'Nombre', value: 'nombre', sortable:true },
                     { text: 'Descripcion', value: 'descripcion', sortable:true },
                     { text: 'Actions', value: 'action', sortable:false },   
-                ],
-                desserts: [],
+                ],                
                 editedIndex: -1,
-                editedItem: {
-                    name: '',
-                    calories: 0,
-                    fat: 0,
-                    carbs: 0,
-                    protein: 0
-                },
-                defaultItem: {
-                    name: '',
-                    calories: 0,
-                    fat: 0,
-                    carbs: 0,
-                    protein: 0
-                }
+                _id:'',
+                nombre:'',
+                descripcion:''
             }
         },
         computed: {
             formTitle () {
-            return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+                return this.editedIndex === -1 ? 'Nuevo registro' : 'Editar registro'
             }
         },
         watch: {
             dialog (val) {
-            val || this.close()
+                val || this.close()
             }
         },
         created () {
@@ -133,33 +114,46 @@
                     console.log(error);
                 })
             },
+            limpiar(){
+                this.nombre='';
+                this.descripcion='';
+            },
+            guardar(){
+                let me = this;
+                if(this.editedIndex > -1){
+                    //Editar registro
+                }else{
+                    //Guardar registro
+                    axios.post('tipos/tipos_lavados/',{'nombre':this.nombre, 'descripcion':this.descripcion})
+                    .then(function (response){
+                        me.limpiar();
+                        me.close();
+                        me.listar();
+
+                    })
+                    .catch(function(error){
+                        console.log(error)
+                    });
+                }
+            },
             editItem (item) {
-            this.editedIndex = this.desserts.indexOf(item)
-            this.editedItem = Object.assign({}, item)
-            this.dialog = true
+                this.editedIndex = this.desserts.indexOf(item)
+                this.editedItem = Object.assign({}, item)
+                this.dialog = true
             },
 
             deleteItem (item) {
-            const index = this.desserts.indexOf(item)
-            confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
+                const index = this.desserts.indexOf(item)
+                confirm('Are you sure you want to delete this item?') && this.desserts.splice(index, 1)
             },
 
             close () {
-            this.dialog = false
-            setTimeout(() => {
-                this.editedItem = Object.assign({}, this.defaultItem)
-                this.editedIndex = -1
-            }, 300)
-            },
-
-            save () {
-            if (this.editedIndex > -1) {
-                Object.assign(this.desserts[this.editedIndex], this.editedItem)
-            } else {
-                this.desserts.push(this.editedItem)
-            }
-            this.close()
-            }
+                this.dialog = false
+                setTimeout(() => {
+                    this.editedItem = Object.assign({}, this.defaultItem)
+                    this.editedIndex = -1
+                }, 300)
+            },            
         }
     }
 </script>
