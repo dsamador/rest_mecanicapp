1fb4253fd35de50896aedfdafca1bd2c0624b1c8
David Amador <54488581+dsamador@users.noreply.github.com>
2021-05-02 07:32:35 -0500

    Se añaden más modelos

--- gestion/models.py
+++ gestion/models.py
@@ -4,7 +4,7 @@ from simple_history.models import HistoricalRecords
 # Create your models here.
 
 
-""" ##########  1  ########## """
+""" ##########  1  ########## +"""
 class TipoLavado(Comunes):
 
     historical = HistoricalRecords()
@@ -25,7 +25,7 @@ class TipoLavado(Comunes):
         return self.nombre
         
 
-""" ##########  2  ########## """
+""" ##########  2  ########## +"""
 class TipoMantenimiento(Comunes):
 
     historical = HistoricalRecords()
@@ -46,7 +46,7 @@ class TipoMantenimiento(Comunes):
         return self.nombre
         
 
-""" ##########  3  ########## """
+""" ##########  3  ########## +"""
 class TipoCombustible(Comunes):
 
     historical = HistoricalRecords()
@@ -133,3 +133,78 @@ class Lavadero(Local):
     def __str__(self):
         return self.nombre
         
+
+""" ##########  7  ########## """
+class Taller(Local):
+    
+    historical = HistoricalRecords()
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+    
+    class Meta:
+        verbose_name = 'Taller'
+        verbose_name_plural = 'Talleres'               
+
+    def __str__(self):
+        return self.nombre
+        
+
+""" ##########  8  ########## """
+class Gasolinera(Local):
+    
+    historical = HistoricalRecords()
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+    
+    class Meta:
+        verbose_name = 'Gasolinera'
+        verbose_name_plural = 'Gasolineras'               
+
+    def __str__(self):
+        return self.nombre
+        
+
+class Vehiculo(BaseModel):
+    
+    nombre = CharField('Nombre del vehículo', max_length=100, blank=False, null=False)
+    modelo = CharField('Modelo', max_length=100, blank=False, null=False)
+    placa = CharField('Placa', unique=True, max_length=10, blank=False, null=False)
+    anio = CharField('Año', max_length=4, blank=False, null=False)    
+    color = CharField('Color', max_length=40, blank=True, null=True)
+    tanque = IntegerField('Capacidad Tanque', blank=False, null=False)
+    num_chasis = CharField('Numero del Chasis', unique=True, max_length=100, blank=True, null=True)
+    VIN = CharField('VIN', unique=True, max_length=45, blank=True, null=True)
+    matricula = CharField('Matrícula', unique=True, max_length=45, blank=True, null=True)
+    tipo = ForeignKey(TipoVehiculo, on_delete = PROTECT)
+    marca = ForeignKey(MarcaVehiculo, on_delete = PROTECT)    
+    #imagen = ImageField('Imagen del vehiculo', upload_to='vehiculos/%Y/%m/%d', height_field=None, width_field=None, max_length=None, blank=True, null=True)
+    fecha = DateTimeField(auto_now_add=True)
+     
+    historical = HistoricalRecords()
+
+    @property
+    def _history_user(self):
+        return self.changed_by
+
+    @_history_user.setter
+    def _history_user(self, value):
+        self.changed_by = value
+    
+    class Meta:
+        verbose_name = 'Vehiculo'
+        verbose_name_plural = 'Vehiculos'               
+
+    def __str__(self):
+        return self.nombre
